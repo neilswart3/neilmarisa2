8a5ee616ef055b57559f6608d0fd5ad8846d2156
Neil Swart <neilswart3@gmail.com>
2020-09-17 08:23:44 +0200

    fix for deploy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import App from 'next/app';
+import App, { AppProps } from 'next/app';
 import { CssBaseline } from '@material-ui/core';
 import { StylesProvider } from '@material-ui/core/styles';
 
-class MyApp extends App<{}> {
+class MyApp extends App<AppProps> {
   render() {
     const { Component, pageProps } = this.props;
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,8 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 // import colors from 'src/app/theme/colors';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
@@ -12,7 +12,6 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta name='mobile-web-app-capable' content='yes;' />
-          <link rel='icon' href='browser.png' type='image/png' />
           <link
             href='https://fonts.googleapis.com/css2?family=Roboto&display=swap'
             rel='stylesheet'
